886dc1ccdbc9ea4835df98a5a1211bbe85722ff6
LittlePien <abdulmalikfajar14@gmail.com>
2026-06-22 21:40:52 +0800

    delete for now

--- RoomAssignment/app/src/main/java/com/example/roomassignment/navigation/NavRoutes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.roomassignment.navigation
-
-object NavRoutes {
-    const val HOME = "home"
-    const val DETAIL = "detail/{bookId}"
-
-    fun detail(bookId: Int) = "detail/$bookId"
-}
